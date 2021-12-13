66f2048bc1472732368a07516b536c10f68b9da7
P41310 <pabloximena291@gmail.com>
2021-12-12 18:52:10 -0600

    commit: trabajando

--- js/funciones.js
+++ js/funciones.js
@@ -34,7 +34,7 @@ const btnRight = document.querySelector("#btn-right");
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next(){
-	let sliderSectionFirst = document.querySelectorAll(".slide__section")[0];
+	let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
 	slider.style.marginLeft = "-200%";
 	slider.style.Transition = "all 0.5s";
 	setTimeout(function(){
@@ -45,8 +45,8 @@ function Next(){
 }
 
 function Prev() {
-	sliderSection = document.querySelectorAll(".slider__section");
-	sliderSectionLast =  sliderSection[sliderSection.length -1];
+	let sliderSection = document.querySelectorAll(".slider__section");
+	let sliderSectionLast =  sliderSection[sliderSection.length -1];
 	slider.style.marginLeft = "0";
 	slider.style.Transition = "all 0.5s";
 	setTimeout(function(){
